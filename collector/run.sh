c4fac4d779aefb90a21be1aa9ce0fb63be0c9ffd
Giannis Neokleous <giannis@knewton.com>
2015-04-10 09:55:57 -0400

    Fixing kafka broker ports

--- collector/run.sh
+++ collector/run.sh
@@ -5,5 +5,5 @@ scala \
     -com.twitter.finagle.tracing.debugTrace=true\
     -zipkin.kafka.groupid=0\
     -zipkin.zookeeper.location=host1:2181,host2:2181,host3:2181/Kafka08\
-    -zipkin.kafka.server=host1:9160,host2:9160,host3:9160\
+    -zipkin.kafka.server=host1:9092,host2:9092,host3:9092\
     -zipkin.kafka.topics=zipkin-tdist=1
